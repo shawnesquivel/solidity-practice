af0ceec97b2ed095200be49d196d52f964f33203
shawnesquivel <shawnesquivel24@gmail.com>
2022-12-07 14:36:37 -0800

    Update FundMe.sol

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -7,17 +7,23 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe { 
 
+    // lets keep track of the funders
+    address[] public funders;
+    mapping(address => uint256) public addressToAmountFunded;
+
     // contracts + wallet can hold tokens! :) 
     function fund() public payable {
         // to get the $ amount -> msg.value;
-        uint256 minUsd = 50;
+        uint256 minUsd = 50 * 1e18;
 
         // Use decentralized oracle net (ChainLink) to convert USD to ETH
 
         // 1*10^18 gwei = 1 ETH
-        require (msg.value >= minUsd, "Did not reach min. 1ETH fund");
+        require (getConversionRate(msg.value) >= minUsd, "Did not reach min. 1ETH fund");
 
         // revert - undoes any actions and sends gas back
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = msg.value;
     }
 
         // We could make this public, but then we'd have to deploy it
@@ -34,10 +40,12 @@ contract FundMe {
         // return uint256(answer * 1e10); // 1* 10 ** 10 == 10000000000
     }
 
-
-    function getConversionRate() public {
-        Ag
-
+    // convert msg.value to dollars
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        // Get the price in ETH/USD
+        uint256 ethPrice = getPrice(); // usd/eth
+        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
+        return ethAmountInUsd;
     }
 
 
