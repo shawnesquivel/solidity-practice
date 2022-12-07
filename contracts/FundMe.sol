b24332f3db3ab58de713c6b552770028e23074bf
shawnesquivel <shawnesquivel24@gmail.com>
2022-12-06 16:15:30 -0800

    Create FundMe.sol

---
+++ contracts/FundMe.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+contract FundMe { 
+
+    // contracts + wallet can hold tokens! :) 
+    function fund() public payable {
+        // to get the $ amount -> msg.value;
+        uint256 minUsd = 50;
+
+        // Use decentralized oracle net (ChainLink) to convert USD to ETH
+
+        // 1*10^18 gwei = 1 ETH
+        require (msg.value >= minUsd, "Did not reach min. 1ETH fund");
+
+        // revert - undoes any actions and sends gas back
+    }
+    // function withdraw() private {
+
+
+    // }
+
+
+}
